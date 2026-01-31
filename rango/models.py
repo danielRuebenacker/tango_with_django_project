c0e6e9cfb159239f8e9b983e57944c74654c66d6
Daniel R <2902294r@student.gla.ac.uk>
2026-01-31 10:04:46 +0000

    Correctly pluralise >>Categories<<

--- rango/models.py
+++ rango/models.py
@@ -7,6 +7,11 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
+    # corectly pluralise
+    class Meta: 
+        verbose_name_plural = 'Categories'
+
+
     def __str__(self):
         return self.name
 
